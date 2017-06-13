b1a056dec071098798b66a140abaa4f48b28f076
Akhyar Ahmed <nuhasahmed007@gmail.com>
2017-06-13 20:21:09 +0600

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Restaurant Application :) 
+                    Restaurant App :) 
                 </div>
 
                 <div class="links">
