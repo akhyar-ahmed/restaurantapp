69fccb277aa8c81ef4f947424e641266efa73340
nabz93 <mrnmrn93@gmail.com>
2017-06-21 22:37:37 +0600

    smille
    smille add after restaurent app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Restaurant App 
+                    Restaurant App :) 
                 </div>
 
                 <div class="links">
