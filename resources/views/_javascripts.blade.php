980efe4b31b65afa4c2593911ef60a175f778e81
Akhyar Ahmed <nuhasahmed007@gmail.com>
2017-07-08 11:51:32 +0600

    bug fixes

--- resources/views/_javascripts.blade.php
+++ resources/views/_javascripts.blade.php
@@ -1,4 +1,4 @@
-  <script src="{{ asset('js/app.js') }}"></script>
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
