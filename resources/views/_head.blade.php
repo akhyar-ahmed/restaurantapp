f1bccfe58dece8fa8ee2c710f6affa48ea75f73f
nabz93 <mrnmrn93@gmail.com>
2017-07-08 11:41:00 +0600

    nab var margin

--- resources/views/_head.blade.php
+++ resources/views/_head.blade.php
@@ -21,6 +21,7 @@
         font-family: 'Raleway', sans-serif;
         font-size: 14px;
       }
+      
     </style>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
