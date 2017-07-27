1a7e10c211174a53bd602f57796b3b4b0d985088
nabz93 <mrnmrn93@gmail.com>
2017-07-28 02:03:46 +0600

    item add in take way

--- database/seeds/TawayItemsSeeder.php
+++ database/seeds/TawayItemsSeeder.php
@@ -15,9 +15,98 @@ class TawayItemsSeeder extends Seeder
         $data = [
 
             [
-                'name' => "bla bla",
-                'category' => "grill",
-                'base_price' => '1.00'
+                'name' => "Fresh Orange Juice",
+                'category' => "Drinks & Desert",
+                'base_price' => '3.00'
+            ],
+
+            [
+                'name' => "Fresh Carrot Juice",
+                'category' => "Drinks & Desert",
+                'base_price' => '3.00'
+            ],
+
+            [
+                'name' => "Fresh Apple Juice",
+                'category' => "Drinks & Desert",
+                'base_price' => '3.00'
+            ],
+
+            [
+                'name' => "Fresh Lemonade",
+                'category' => "Drinks & Desert",
+                'base_price' => '3.00'
+            ],
+
+            [
+                'name' => "Mix Fresh Juice",
+                'category' => "Drinks & Desert",
+                'base_price' => '3.00'
+            ],
+
+            [
+                'name' => "English Tea",
+                'category' => "Drinks & Desert",
+                'base_price' => '1.50'
+            ],
+            [
+                'name' => "Chamomile Tea",
+                'category' => "Drinks & Desert",
+                'base_price' => '1.50'
+            ],
+
+            [
+                'name' => "Jeanine Tea",
+                'category' => "Drinks & Desert",
+                'base_price' => '1.50'
+            ],
+
+            [
+                'name' => "Pippermint Tea",
+                'category' => "Drinks & Desert",
+                'base_price' => '1.50'
+            ],
+
+            [
+                'name' => "Green Tea",
+                'category' => "Drinks & Desert",
+                'base_price' => '1.50'
+            ],
+
+            [
+                'name' => "Coffie Latte",
+                'category' => "Drinks & Desert",
+                'base_price' => '2.00'
+            ],
+
+            [
+                'name' => "Cappuccino",
+                'category' => "Drinks & Desert",
+                'base_price' => '2.00'
+            ],
+
+            [
+                'name' => "American Coffie",
+                'category' => "Drinks & Desert",
+                'base_price' => '2.00'
+            ],
+
+            [
+                'name' => "Espresso Coffie",
+                'category' => "Drinks & Desert",
+                'base_price' => '2.00'
+            ],
+
+            [
+                'name' => "Hot Chocolate",
+                'category' => "Drinks & Desert",
+                'base_price' => '2.00'
+            ],
+
+            [
+                'name' => "Banana Shake",
+                'category' => "Drinks & Desert",
+                'base_price' => '2.50'
             ],
 
             [
