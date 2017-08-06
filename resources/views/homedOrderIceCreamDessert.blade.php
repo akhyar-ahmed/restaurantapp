a3d4b2605bb402a09aafb0ff1031567235a0a095
nabz93 <mrnmrn93@gmail.com>
2017-08-07 01:35:06 +0600

    home delivery added as homedOrder

---
+++ resources/views/homedOrderIceCreamDessert.blade.php
@@ -0,0 +1,221 @@
+@extends('main')
+
+@section('title', "| TakeAway-Orders ")
+
+@section('content')
+<div class="container">
+    <div class = "page-header container col-xs-12 form-group">
+		<h1 style="text-align:left; display:inline">Food Ordering - </h1><h2 style="text-align:center; display:inline">(Place Your Customer Order Here)</h3>
+	</div>
+
+	<div id="result" class = "container form-group col-xs-12">
+    
+    </div>
+    <div class="card text-center">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs" >
+            <li class="nav-item" id="mitem">
+                <a class="TItem" href="#" style="color:white">Drinks</a>
+            </li>
+            <li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Dessert & Ice Cream</a>
+            </li>
+            <li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Sides</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Pizza</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Curry</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Shawarma</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Wrap</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Burgers</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Grilled</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white"> Salads & cold Mezze</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Special deals</a>
+            </li>
+            </ul>
+        </div>
+		<br>
+        <div class="container card-block">
+
+			<h3 align="left"><u>Desserts</u></h3><br/>
+			<div class="row-xs-12" >
+
+				<div class="col-xs-12">
+					<div class="card">
+						<img class="card-img-top" src="" alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Cakes</h5>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox" value="">White fruits</label>
+                            </div>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox" value="">Choco choco cream</label>
+                            </div>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox" value="">Tarte strawberry</label>
+                            </div>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox" value="">Termeso</label>
+                            </div>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox" value="">Chocolate fudge cake</label>
+                            </div>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox" value="">Cheesecake</label>
+                            </div>
+                            <a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+                            <a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+
+        <div class="container card-block">
+
+			<h3 align="left"><u>Desserts</u></h3><br/>
+			<div class="row-xs-12" >
+
+				<div class="col-xs-12">
+					<div class="card">
+						<img class="card-img-top" src="" alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Ice-Creams</h5>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox-inline" value="">Chocolate ice cream</label>
+                            </div>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox-inline" value=""> Banana funnel ice cream </label>
+                            </div>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox-inline" value="">Kiwi ice cream</label>
+                            </div>
+                            <div class="checkbox-inline">
+                                <label><input type="checkbox-inline" value="">Venilla ice cream</label>
+                            </div>
+                            
+                            <a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+                            <a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+
+
+		
+    </div>
+</div>
+
+@endsection
+
+@push('scripts')
+<script>
+	$(document).ready(function(){
+		$('#search_text').keyup(function(){
+			var txt = $(this).val();
+			if( txt != '') {
+				$.ajaxSetup({
+        		headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					}
+				});
+				$.ajax({
+					url: "/food-orders/searchfood",
+					method: "POST",
+					data: {
+						search:txt
+						},
+					dataType: "text",
+					success: function(data){
+						$('#result').html(data);
+					}
+				});
+
+			}
+			else {
+				$('#result').html('');
+
+				$.ajaxSetup({
+        		headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					}
+				});
+				$.ajax({
+					url: "/food-orders/searchfood",
+					method: "POST",
+					data: {
+						search:txt
+						},
+					dataType: "text",
+					success: function(data){
+						$('#result').html(data);
+					}
+				});
+			}
+		});
+		$('#search_text').keydown(function(){
+			var txt = $(this).val();
+			if( txt != '') {
+				$.ajaxSetup({
+        		headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					}
+				});
+				$.ajax({
+					url: "/food-orders/searchfood",
+					method: "POST",
+					data: {
+						search:txt
+						},
+					dataType: "text",
+					success: function(data){
+						$('#result').html(data);
+					}
+				});
+
+			}
+			else {
+				$('#result').html('');
+				
+				$.ajaxSetup({
+        		headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					}
+				});
+				$.ajax({
+					url: "/food-orders/searchfood",
+					method: "POST",
+					data: {
+						search:txt
+						},
+					dataType: "text",
+					success: function(data){
+						$('#result').html(data);
+					}
+				});
+			}
+		});
+	});
+</script>
+@endpush
+
---
+++ resources/views/homedOrderSaladsMezze.blade.php
@@ -0,0 +1,284 @@
+@extends('main')
+
+@section('title', "| TakeAway-Orders ")
+
+@section('content')
+<div class="container">
+    <div class = "page-header container col-xs-12 form-group">
+		<h1 style="text-align:left; display:inline">Food Ordering - </h1><h2 style="text-align:center; display:inline">(Place Your Customer Order Here)</h3>
+	</div>
+
+	<div id="result" class = "container form-group col-xs-12">
+    
+    </div>
+    <div class="card text-center">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs" >
+            <li class="nav-item" id="mitem">
+                <a class="TItem" href="#" style="color:white">Drinks</a>
+            </li>
+            <li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Dessert & Ice Cream</a>
+            </li>
+            <li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Sides</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Pizza</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Curry</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Shawarma</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Wrap</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Burgers</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Grilled</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white"> Salads & cold Mezze</a>
+            </li>
+			<li class="nav-item">
+                <a class="TItem" href="#" style="color:white">Special deals</a>
+            </li>
+            </ul>
+        </div>
+		<br>
+        <div class="container card-block">
+
+			<h3 align="left"><u>Salads</u></h3><br/>
+			<div class="row-xs-12" >
+
+				<div class="col-xs-2">
+					<div class="card">
+						<img class="card-img-top" src="" alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title"> Fattoush </h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-2">
+					<div class="card" >
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Chicken Caesar salad</h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-2">
+					<div class="card" >
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Green salad </h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-2">
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Tabula</h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-xs-2">
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Greek Salad</h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+                
+
+				
+			</div>
+
+			
+
+			
+
+        </div>
+
+        <div class="container card-block">
+            <h3 align="left"><u>Cold Mezze</u></h3><br/>
+            <div class="row-xs-12">
+                <div class="col-xs-2">
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Hummus</h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+                <div class="col-xs-2">
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Moutabil</h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+                <div class="col-xs-2">
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Coleslaw</h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+                <div class="col-xs-2">
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title"> Mix olives</h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+                <div class="col-xs-2">
+					<div class="card">
+						<img class="card-img-top" src="..." alt="Card image cap">
+						<div class="card-block">
+							<h5 class="card-title">Jagic</h5>
+							<a href="#" class="btn btn-success"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></a>
+							<a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></a>
+						</div>
+					</div>
+				</div>
+
+            </div>
+        </div>
+
+		
+
+
+		
+    </div>
+</div>
+
+@endsection
+
+@push('scripts')
+<script>
+	$(document).ready(function(){
+		$('#search_text').keyup(function(){
+			var txt = $(this).val();
+			if( txt != '') {
+				$.ajaxSetup({
+        		headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					}
+				});
+				$.ajax({
+					url: "/food-orders/searchfood",
+					method: "POST",
+					data: {
+						search:txt
+						},
+					dataType: "text",
+					success: function(data){
+						$('#result').html(data);
+					}
+				});
+
+			}
+			else {
+				$('#result').html('');
+
+				$.ajaxSetup({
+        		headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					}
+				});
+				$.ajax({
+					url: "/food-orders/searchfood",
+					method: "POST",
+					data: {
+						search:txt
+						},
+					dataType: "text",
+					success: function(data){
+						$('#result').html(data);
+					}
+				});
+			}
+		});
+		$('#search_text').keydown(function(){
+			var txt = $(this).val();
+			if( txt != '') {
+				$.ajaxSetup({
+        		headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					}
+				});
+				$.ajax({
+					url: "/food-orders/searchfood",
+					method: "POST",
+					data: {
+						search:txt
+						},
+					dataType: "text",
+					success: function(data){
+						$('#result').html(data);
+					}
+				});
+
+			}
+			else {
+				$('#result').html('');
+				
+				$.ajaxSetup({
+        		headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					}
+				});
+				$.ajax({
+					url: "/food-orders/searchfood",
+					method: "POST",
+					data: {
+						search:txt
+						},
+					dataType: "text",
+					success: function(data){
+						$('#result').html(data);
+					}
+				});
+			}
+		});
+	});
+</script>
+@endpush
+
